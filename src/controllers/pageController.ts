e82227335025f466162b8bfe659b3af4ca49e438
Cleyton Mendes <cleyton.mendest@gmail.com>
2022-01-17 21:40:14 -0300

    incluindo views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,19 @@
 import {Request, Response} from 'express'
 
 export const home = (req:Request, res:Response) =>{
-    //res.render('pages/page')
+    res.render('pages/pages')
 }
 
 export const dogs = (req:Request, res:Response) =>{
-    //res.render('pages/page')
+    res.send('dogs')
 }
 
 export const cats = (req:Request, res:Response) =>{
+    res.send('cats')
     //res.render('pages/page')
 }
 
 export const fishes = (req:Request, res:Response) =>{
+    res.send('fishes')
     //res.render('pages/page')
 }
